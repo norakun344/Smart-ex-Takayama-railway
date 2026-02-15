90360e4b0b1587e848f18f375f80549ebbf6affd
norakun344 <cupcake.nora4649@icloud.com>
2026-02-15 22:39:14 +0900

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+<script type="module">
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+
+import { getFirestore } from 
+"https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJXGMsipD4chYb_jm7OkYRdIoOubQbak0",
+  authDomain: "takayama-smartex.firebaseapp.com",
+  projectId: "takayama-smartex"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+</script>
