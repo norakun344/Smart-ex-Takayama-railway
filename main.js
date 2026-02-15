64e037e23c1182a4380011d3cec2f2e1be76d1bb
norakun344 <cupcake.nora4649@icloud.com>
2026-02-15 22:52:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
 import { db } from "./firebase.js";
 
-console.log("Firestore connected:", db);
-
-// 例：ボタンを押したら動く処理
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("testBtn");
-  if (btn) {
-    btn.addEventListener("click", () => {
-      alert("動いた！");
-    });
-  }
-});
+console.log("Firebase connected:", db);
